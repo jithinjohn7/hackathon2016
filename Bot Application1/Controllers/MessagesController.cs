69dea2a8464168ee12c604d194b0671e1cb87972
kak <kak@DVCIO2630754>
2016-11-12 02:10:43 +0530

    hi

--- Bot Application1/Controllers/MessagesController.cs
+++ Bot Application1/Controllers/MessagesController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace Bot_Application1
 {
@@ -24,9 +26,13 @@ namespace Bot_Application1
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
                 // calculate something for us to return
                 int length = (activity.Text ?? string.Empty).Length;
-
+                string  message = activity.Text;
+                Task<dynamic> re = MakeRequestToTASAPI(message);
+                dynamic res  =  await re;
                 // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                //Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                Activity reply = activity.CreateReply($"{res.Score}");
+
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
@@ -65,5 +71,45 @@ namespace Bot_Application1
 
             return null;
         }
+
+        async Task<dynamic> MakeRequestToTASAPI(string message)
+        {
+            try
+            {
+                string myComment = message.Replace(' ', '+');
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{3c8a96c7-6639-4739-ac98-da2e7203e93f}");
+                var uri = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + myComment;
+
+                byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+                HttpResponseMessage response;
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("< your content type, i.e. application/json >");
+                    response = await client.PostAsync(uri, content);
+                }
+                dynamic product = null;
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = response.Content.ReadAsStringAsync().Result;
+                    product = JsonConvert.DeserializeObject(data);
+                    return product.Score;
+                }
+                else
+                {
+                    message = product.ReasonPhrase;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return message;
+        }
+
+            
+
+        }
     }
-}
---
+++ Bot Application1/Logic Apps/TextAnalytics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+
+namespace Bot_Application1.Logic_Apps
+{
+    public class TextAnalytics
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{subscription key}");
+
+            // Request parameters
+            queryString["numberOfLanguagesToDetect"] = "{integer}";
+            var uri = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("< your content type, i.e. application/json >");
+                response = await client.PostAsync(uri, content);
+            }
+
+        }
+    }
+}
